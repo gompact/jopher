4c3beaa8285dbd8cb87602e625e89f72a53a5c09
Islam Shehata <schehata91@gmail.com>
2019-11-24 01:36:09 +0100

    Added test for Create()

--- jopher_test.go
+++ jopher_test.go
@@ -22,22 +22,54 @@ func TestSuccess(t *testing.T) {
 	if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
 		t.Log(err.Error())
 	}
-
 	defer resp.Body.Close()
 
 	// parse body as json
 	if body["Hello"] != "World" {
-		t.Log("Failed to get correct values from response body")
-		t.Fail()
+		t.Errorf("failed to get correct values from response body")
+		return
 	}
 
-	if resp.Status != "OK" {
-		t.Log("Response status code != 200")
-		t.Fail()
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("expected response status code to be 200, found %d", resp.StatusCode)
+		return
 	}
 
 	if !strings.Contains(resp.Header.Get("Content-Type"), "application/json") {
-		t.Log("Response header Content-Type should be application/json")
-		t.Fail()
+		t.Errorf("Response header Content-Type should be application/json")
+		return
+	}
+}
+
+func TestCreated(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		Created(w, M{"Hello": "World"})
+	}
+
+	req := httptest.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	resp := w.Result()
+	body := make(map[string]string)
+	if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
+		t.Log(err.Error())
+	}
+	defer resp.Body.Close()
+
+	// parse body as json
+	if body["Hello"] != "World" {
+		t.Errorf("failed to get correct values from response body")
+		return
+	}
+
+	if resp.StatusCode != http.StatusCreated {
+		t.Errorf("expected response status code to be 201, found %d", resp.StatusCode)
+		return
+	}
+
+	if !strings.Contains(resp.Header.Get("Content-Type"), "application/json") {
+		t.Errorf("Response header Content-Type should be application/json")
+		return
 	}
 }
